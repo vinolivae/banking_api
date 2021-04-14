9ef98f00702110762a5a323fe17846da391850b1
vinolivae <vinicius.anjos@stone.com.br>
2021-04-14 15:10:05 -0300

    chore: tidy up config

--- apps/banking_api/mix.exs
+++ apps/banking_api/mix.exs
@@ -9,7 +9,7 @@ defmodule BankingApi.MixProject do
       config_path: "../../config/config.exs",
       deps_path: "../../deps",
       lockfile: "../../mix.lock",
-      elixir: "~> 1.7",
+      elixir: "~> 1.11",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
       aliases: aliases(),
