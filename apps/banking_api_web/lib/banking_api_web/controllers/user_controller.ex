663449cdd63082291368425c932231dfdc2b3a1c
vinolivae <vinicius.anjos@stone.com.br>
2021-04-20 12:16:36 -0300

    refact: refactory some files

--- apps/banking_api_web/lib/banking_api_web/controllers/user_controller.ex
+++ apps/banking_api_web/lib/banking_api_web/controllers/user_controller.ex
@@ -11,19 +11,4 @@ defmodule BankingApiWeb.UserController do
       |> render("create.json", user: user)
     end
   end
-
-  def show_all(conn, _params) do
-    {:ok, users} = BankingApi.fetch_all_users()
-
-    maped_users =
-      Enum.map(users, &Map.from_struct/1) |> Enum.map(&Map.drop(&1, [:account, :__meta__]))
-
-    send_json(conn, 200, maped_users)
-  end
-
-  defp send_json(conn, status, body) do
-    conn
-    |> put_resp_header("content-type", "application/json")
-    |> send_resp(status, Jason.encode!(body))
-  end
 end
