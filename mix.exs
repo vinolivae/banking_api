ad69440e41d134a824fa6a05fe1fcbde84858aa7
vinolivae <vinicius.anjos@stone.com.br>
2021-07-31 01:29:54 -0300

    chore: add excoveralls

--- mix.exs
+++ mix.exs
@@ -7,7 +7,14 @@ defmodule BankingApi.Umbrella.MixProject do
       version: "0.1.0",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
-      aliases: aliases()
+      aliases: aliases(),
+      test_coverage: [tool: ExCoveralls],
+      preferred_cli_env: [
+        coveralls: :test,
+        "coveralls.detail": :test,
+        "coveralls.post": :test,
+        "coveralls.html": :test
+      ]
     ]
   end
 
@@ -25,7 +32,8 @@ defmodule BankingApi.Umbrella.MixProject do
   # and cannot be accessed from applications inside the apps/ folder.
   defp deps do
     [
-      {:credo, "~> 1.4", only: [:dev, :test], runtime: false}
+      {:credo, "~> 1.4", only: [:dev, :test], runtime: false},
+      {:excoveralls, "~> 0.14.2", olny: :test}
     ]
   end
 
